01c564b268c4fdb1fff4071735a47a69591fcaa1
Leo Maskell <22maskelll@bluecoatstudent.org.uk>
2025-08-30 16:51:42 +0000

    STABLE added some styles etc

--- server.js
+++ server.js
@@ -2,4 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-console.log(`app: \n\ ${app} \n\ \n\ port: \n\ ${port}`);
+
+app.use(express.json());
+
+// pages
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/frontend/index.html");
+});
+
+// styles
+app.get("/style.css", (req, res) => {
+    res.sendFile(__dirname + "/static/style.css")
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
+});
